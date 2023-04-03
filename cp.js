3c998742223abc2ac450966920e921363c44d1f2
leandro Morais <privado.morais@gmail.com>
2023-04-03 11:20:44 -0300

    utlizando for

--- cp.js
+++ cp.js
@@ -1,3 +1,4 @@
+/*switch
 let opção = parseInt(prompt("digite uma opção"));
 
 switch(opção){
@@ -13,4 +14,29 @@ switch(opção){
     default:
         document.write("caso nao seja a primeira opção e nem a segunda opção")
         break;
+}
+*/
+
+/*incremento e decremento
+let numero = 7
+document.write(numero)
+valor = numero+1
+document.write("<br><br>")
+numero = numero+1
+document.write(numero)
+*/
+
+/*decrementando valores
+let numero1 = 7
+document.write(numero1)
+valor = numero-1
+document.write("<br><br>")
+numero1 = numero1
+*/
+
+
+let carros=["gol","fusca","hb20"]
+for(let i=0; i<carros.length;i++){
+    document.write(carros[i]);
+    document.write("<br><br>")
 }
