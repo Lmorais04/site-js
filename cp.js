56ef4934b4b43168bb57f16f5be07ce4a1e96788
leandro Morais <privado.morais@gmail.com>
2023-04-03 10:44:33 -0300

    utilizando switch

---
+++ cp.js
@@ -0,0 +1,16 @@
+let opção = parseInt(prompt("digite uma opção"));
+
+switch(opção){
+    case 1:
+        document.write("primeira opção")
+        break;
+    case 2:
+        document.write("segunda opção")
+        break;
+    case 3:
+        document.write("terceira opção")
+        break;
+    default:
+        document.write("caso nao seja a primeira opção e nem a segunda opção")
+        break;
+}
